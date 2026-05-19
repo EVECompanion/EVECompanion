f55331163ac5290a1bc7852c33a4bb70a5149e3c
Jonas Schlabertz <jonas@schlabertz.de>
2026-05-19 22:02:05 +0200

    Adds 2d positions of solar systems to SDE

--- SDEConverter/Tables/SolarSystemsTable.swift
+++ SDEConverter/Tables/SolarSystemsTable.swift
@@ -21,6 +21,8 @@ class SolarSystemsTable: SDETable {
     var xColumn = Expression<Float64>("x")
     var yColumn = Expression<Float64>("y")
     var zColumn = Expression<Float64>("z")
+    var position2dxColumn = Expression<Float64?>("2dx")
+    var position2dyColumn = Expression<Float64?>("2dy")
     
     func addColumns(to table: SQLite.TableBuilder) {
         table.column(solarSystemIdColumn)
@@ -32,6 +34,8 @@ class SolarSystemsTable: SDETable {
         table.column(xColumn)
         table.column(yColumn)
         table.column(zColumn)
+        table.column(position2dxColumn)
+        table.column(position2dyColumn)
     }
     
     func createIndexes(connection: Connection) throws {
@@ -54,6 +58,22 @@ class SolarSystemsTable: SDETable {
             sunTypeId = nil
         }
         
+        let position2d = data["position2D"] as? [String: Any]
+        
+        let position2dx: Float64?
+        if let position2dxDouble = position2d?["x"] as? Double {
+            position2dx = Float64(position2dxDouble)
+        } else {
+            position2dx = nil
+        }
+        
+        let position2dy: Float64?
+        if let position2dyDouble = position2d?["y"] as? Double {
+            position2dy = Float64(position2dyDouble)
+        } else {
+            position2dy = nil
+        }
+        
         try db.run(
             table.insert(
                 solarSystemIdColumn <- Int64(id),
@@ -64,7 +84,9 @@ class SolarSystemsTable: SDETable {
                 sunTypeIdColumn <- sunTypeId,
                 xColumn <- Float64(position["x"] as! Double),
                 yColumn <- Float64(position["y"] as! Double),
-                zColumn <- Float64(position["z"] as! Double)
+                zColumn <- Float64(position["z"] as! Double),
+                position2dxColumn <- position2dx,
+                position2dyColumn <- position2dy
             )
         )
     }
