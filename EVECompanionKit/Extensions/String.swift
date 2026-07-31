1e33d4758e6d32cd0a5fcdc287df49657dd93db0
Jonas Schlabertz <jonas@schlabertz.de>
2026-07-31 22:01:17 +0200

    Fixes a crash on iOS 16 when opening ship item details.

--- EVECompanionKit/Extensions/String.swift
+++ EVECompanionKit/Extensions/String.swift
@@ -7,62 +7,85 @@
 
 import Foundation
 public import UIKit
+import SwiftSoup
 
 extension String {
 
     public func convertToAttributed() -> AttributedString {
-        guard let data = data(using: .utf16) else {
-          return AttributedString(self)
-        }
-        
-        guard let attributedString = try? NSAttributedString(
-            data: data,
-            options: [.documentType: NSAttributedString.DocumentType.html],
-            documentAttributes: nil
-        ) else {
-            return AttributedString(self)
-        }
-        
-        guard let modifiedAttributedString = attributedString.mutableCopy() as? NSMutableAttributedString else {
-            return AttributedString(attributedString)
-        }
-        
-        attributedString.enumerateAttributes(in: NSRange(location: 0,
-                                                         length: modifiedAttributedString.length)) { attributes, range, _ in
-            for(key, value) in attributes {
-                switch key {
-                    
-                case .font:
-                    modifiedAttributedString.removeAttribute(.font, range: range)
-                    
-                case .foregroundColor:
-                    modifiedAttributedString.removeAttribute(.foregroundColor, range: range)
-                    
-                case .kern:
-                    modifiedAttributedString.removeAttribute(.kern, range: range)
-                    
-                case .link:
-                    guard let valueURL = value as? URL else {
-                        continue
-                    }
-                    
-                    if valueURL.scheme?.lowercased() == "showinfo",
-                       let showInfoComponents = URLComponents(url: valueURL,
-                                                              resolvingAgainstBaseURL: false),
-                       let typeId = Int(showInfoComponents.path),
-                       let url = URL(string: "evecompanion://showinfo/\(typeId)") {
-                        modifiedAttributedString.removeAttribute(.link, range: range)
-                        modifiedAttributedString.addAttribute(.link, value: url, range: range)
-                    }
-                    
-                default:
-                    continue
-                    
+            do {
+                let document = try SwiftSoup.parseBodyFragment(self)
+
+                guard let body = document.body() else {
+                    return AttributedString(self)
                 }
+
+                var result = AttributedString()
+
+                for node in body.getChildNodes() {
+                    result.append(try attributedString(from: node))
+                }
+
+                return result
+
+            } catch {
+                return AttributedString(self)
             }
         }
-        
-        return AttributedString(modifiedAttributedString)
-    }
+
+        private func attributedString(from node: Node) throws -> AttributedString {
+            if let textNode = node as? TextNode {
+                return AttributedString(textNode.getWholeText())
+            }
+
+            if let element = node as? SwiftSoup.Element {
+                let tag = element.tagName()
+
+                switch tag.lowercased() {
+                case "br":
+                    return AttributedString("\n")
+
+                case "b", "strong":
+                    var result = AttributedString()
+                    
+                    for child in element.getChildNodes() {
+                        result.append(try attributedString(from: child))
+                    }
+                    
+                    result.inlinePresentationIntent = .stronglyEmphasized
+                    
+                    return result
+
+                case "a":
+                    var result = AttributedString()
+
+                    for child in element.getChildNodes() {
+                        result.append(try attributedString(from: child))
+                    }
+
+                    let href = try element.attr("href")
+
+                    if href.lowercased().hasPrefix("showinfo:"),
+                       let id = href.split(separator: ":").last,
+                       let url = URL(string: "evecompanion://showinfo/\(id)") {
+                        result.link = url
+                    } else if let url = URL(string: href) {
+                        result.link = url
+                    }
+
+                    return result
+
+                default:
+                    var result = AttributedString()
+
+                    for child in element.getChildNodes() {
+                        result.append(try attributedString(from: child))
+                    }
+
+                    return result
+                }
+            }
+
+            return AttributedString()
+        }
     
 }
